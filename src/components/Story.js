9c43ca7f0ebf3ad26bd81a816792c32b28adea5f
Mariano De Simone <desimone.mariano@gmail.com>
2021-10-12 00:52:00 -0300

    First C3

--- src/components/Story.js
+++ src/components/Story.js
@@ -13,18 +13,20 @@ const Story = ({ id }) => {
 
   return (
     <ListGroup as="ol">
-      <ListGroup.Item
-        as="li"
-        className="d-flex justify-content-between align-items-start"
-      >
-        <div className="ms-2 me-auto">
-          <div className="fw-bold">
-            <a href={story.url}>{story.title}</a>
+      {!story.title ? null : (
+        <ListGroup.Item
+          as="li"
+          className="d-flex justify-content-between align-items-start"
+        >
+          <div className="ms-2 me-auto">
+            <div className="fw-bold">
+              <a href={story.url}>{story.title}</a>
+            </div>
+            {story.score} points - by: {story.by} - posted:{" "}
+            {timeMap(story.time)} ago.
           </div>
-          {story.score} points - by: {story.by} - posted: {timeMap(story.time)}{" "}
-          ago.
-        </div>
-      </ListGroup.Item>
+        </ListGroup.Item>
+      )}
     </ListGroup>
   );
 };
